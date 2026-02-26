544db17112fa9bafe6271bd9cad60d84fc1a248a
Artem Vesnin <artem_vesnin@iszf.irk.ru>
2026-02-26 04:51:51 +0000

    Added definition of device ID via MAC

--- WIFI.h
+++ WIFI.h
@@ -1,7 +1,13 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WiFiMulti.h>
 
 String id() {  
-  //TODO make it from MAC
-  return "";
+  uint8_t mac[WL_MAC_ADDR_LENGTH];
+  WiFi.softAPmacAddress(mac);
+  String macID = String(mac[WL_MAC_ADDR_LENGTH-2], HEX);
+  macID = macID + String(mac[WL_MAC_ADDR_LENGTH-1], HEX);
+  return macID;
 }
 
 void start_AP_mode() {
--- esp_network.ino
+++ esp_network.ino
@@ -2,9 +2,13 @@
 #include "WIFI.h"
 
 void setup() {
+  Serial.begin(115200);
   init_WIFI(WIFI_START_MODE_AP);
 }
 
 // the loop function runs over and over again forever
 void loop() {
+  delay(500);
+  Serial.print("Our id is:");
+  Serial.println(id());
 }
